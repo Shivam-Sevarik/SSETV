97e95620cf409d94d649ded2fab2e4e624b5f4ff
Shivam Sevarik <shivam.sevarik@innoeye.com>
2020-07-09 18:17:39 +0530

    initial commit

---
+++ SSETV/SSETV.h
@@ -0,0 +1,19 @@
+//
+//  SSETV.h
+//  SSETV
+//
+//  Created by Shivam Sevarik on 09/07/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SSETV.
+FOUNDATION_EXPORT double SSETVVersionNumber;
+
+//! Project version string for SSETV.
+FOUNDATION_EXPORT const unsigned char SSETVVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SSETV/PublicHeader.h>
+
+
